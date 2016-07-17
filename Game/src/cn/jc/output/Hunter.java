81982577826a2e606a88f73188d952a36cd1057f
mrdong916 <mrdong916@163.com>
2016-07-17 19:29:11 +0800

    完善输出功能

--- Game/src/cn/jc/output/Hunter.java
+++ Game/src/cn/jc/output/Hunter.java
@@ -4,40 +4,51 @@ package cn.jc.output;
  * @author:Mrdong916
  */
 public class Hunter {
-	String name;
+	String hunterName;
 	int    life;
 	String weapon;
 	boolean isLive;
-
-	void fight(Monster mon){
+	
+	//使用构造函数进行赋值
+	public Hunter(String weapon){
+		this.hunterName = "奥特曼";
+		this.isLive = true;
+		this.life = 100;
+		this.weapon = weapon;
+	}
+	public void fight(Monster mon){
+		
 		//判断hunter是否死亡
 		if(life<=0){
 			return;
 		}
+		
 		//判断Monster是否死亡
 		if(!mon.isLive){
 			return;
 		}
+		
 		//没有死亡，进行攻击,Monster受伤
-		System.out.println(name+"使用"+weapon+"开始进攻");
+		System.out.println(hunterName+"使用"+weapon+"开始进攻");
 		mon.injured();
 		mon.show();
 	}
-	void injured(){
+	public void injured(){
 		life-=20;
-		System.out.println(name+":死怪物，居然敢打我！！！");
+		System.out.println(hunterName+":死怪物，居然敢打我！！！");
 		//判断是否死亡
 		if(life==0){
 			dead();
 		}
 	}
-	void dead(){
-		System.out.println("我"+name+"复活之时，定是你灭亡之日！！！");
+	public void dead(){
+		System.out.println("我"+hunterName+"复活之时，定是你灭亡之日！！！");
 		isLive = false;
 	}
-	void show(){
-		System.out.println("人物："+name+"\n生命值："+life+"\n死亡状态："+!isLive);
-		
+	public void show(){
+		System.out.println("========================");
+		System.out.println(hunterName+"状态");
+		System.out.println("生命值："+life+"\n死亡状态："+!isLive);
 		System.out.println("========================");
 	}
 
--- Game/src/cn/jc/output/Monster.java
+++ Game/src/cn/jc/output/Monster.java
@@ -4,11 +4,29 @@ package cn.jc.output;
  * @author:Mrdong916
  */
 public class Monster {
-	String type;
+	String monsterName;
 	boolean isLive;
 	int life;
+	//构造函数 对怪兽进行初始化
+	public Monster(int monType){
+		//判断哪种类型的怪兽,并对怪兽类型进行初始化
+		if(monType==1){
+			this.monsterName = "小怪兽";
+			this.life = 60;
+			this.isLive = true;
+		}else if(monType==2){
+			this.monsterName = "大怪兽";
+			this.life = 1000;
+			this.isLive = true;
+		}else if(monType==3){
+			this.monsterName = "超级怪兽";
+			this.life = 150;
+			this.isLive = true;
+		}
+	}
 	
-	void kill(Hunter hun){
+	//怪兽进行攻击
+	public void kill(Hunter hun){
 		//判断Monster是否死亡
 		if(life<=0){
 			return;
@@ -18,24 +36,27 @@ public class Monster {
 			return;
 		}
 		//继续攻击，hunter受伤
-		System.out.println(type+"开始进攻");
+		System.out.println(monsterName+"开始进攻");
 		hun.injured();
 		hun.show();
 	}
-	void injured(){
+	public void injured(){
 		life -= 20;
-		System.out.println(type+":噢呜呜呜呜呜呜呜呜~~~");
+		System.out.println(monsterName+":噢呜呜呜呜呜呜呜呜~~~");
 		//判断是否死亡
 		if(life<=0){
 			dead();
 		}
+		//实现自动反击功能kill();
 	}
-	void dead(){
+	public void dead(){
 		System.out.println("呜呜啊啊啊啊~~~");
 		isLive = false;
 	}
-	void show(){
-		System.out.println(type+"\n生命值："+life+"\n死亡状态："+!isLive);
+	public void show(){
+		System.out.println("========================");
+		System.out.println(monsterName+"状态");
+		System.out.println("生命值："+life+"\n死亡状态："+!isLive);
 		System.out.println("========================");
 	}
 }
